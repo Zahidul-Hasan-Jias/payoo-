330de0a52d4b9d493ef76d861b5e40a7a691ded7
Zahidul hasan jias <zahidulhasan887@gmail.com>
2025-11-11 22:11:44 +0600

    add login page

---
+++ script.js
@@ -0,0 +1,21 @@
+// login button funtionality
+
+document.getElementById("LoginButton").addEventListener("click",function(e){
+    e.preventDefault()
+    const mobileNumber =19700557290
+    const pinNumber = 1234
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted=parseInt(pinNumberValue)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber)
+    {
+        window.location.href="./home.html"
+    }
+    else{
+        alert("Invalid Information")
+    }
+
+})
